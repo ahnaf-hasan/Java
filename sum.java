e8c65a0e5940884f95abbdaa1cd05d93c786d84c
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-10-12 22:11:27 +0600

    sum.java

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int result = addNumbers(13, 12);
+        System.out.println("Sum: " + result);
+    }
+
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
