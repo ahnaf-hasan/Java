4c9e2d58f096cc8e89b20c864e193d129a1da846
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-10-13 21:47:50 +0600

    loop.java

---
+++ loop.java
@@ -0,0 +1,8 @@
+public class LoopExample {
+    public static void main(String[] args) {
+        //  print numbers from 1 to 25
+        for (int i = 1; i <= 25; i++) {
+            System.out.println(i);
+        }
+    }
+}
