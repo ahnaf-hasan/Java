d1a6d55f616518c29e4a5ac51fa40441b1e8bc3f
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-10-16 19:48:55 +0600

    Sixth Commit

---
+++ triangle.java
@@ -0,0 +1,15 @@
+public class TrianglePattern {
+    public static void main(String[] args) {
+        int rows = 7; 
+
+        for (int i = 1; i <= rows; i++) {
+
+            // Print stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+          
+            System.out.println();
+        }
+    }
+}
